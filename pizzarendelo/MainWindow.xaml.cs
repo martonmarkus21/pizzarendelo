b9b54cb617a3d5e0fbab9245c7cd5e7f82788e8e
Márkus Márton <markus.marton@petrik.hu>
2025-11-25 13:21:58 +0100

    Stringek létrehozása és feltétek logika megírása

--- pizzarendelo/MainWindow.xaml.cs
+++ pizzarendelo/MainWindow.xaml.cs
@@ -20,5 +20,20 @@ namespace pizzarendelo
         {
             InitializeComponent();
         }
-    }
+
+		private void btnRendeles_Click(object sender, RoutedEventArgs e)
+		{
+			string teszta = (lstTesztak.SelectedItem as ListBoxItem)?.Content.ToString() ?? "nincs kiválasztva";
+			string meret = (cmbMeret.SelectedItem as ComboBoxItem)?.Content.ToString() ?? "nincs kiválasztva";
+			string feltetek = "";
+
+			if (chkSajt.IsChecked == true) feltetek += "Sajt, ";
+			if (chkSonka.IsChecked == true) feltetek += "Sonka, ";
+			if (chkGomba.IsChecked == true) feltetek += "Gomba, ";
+			if (chkOliva.IsChecked == true) feltetek += "Olívabogyó, ";
+			if (chkKukorica.IsChecked == true) feltetek += "Kukorica, ";
+			if (feltetek.EndsWith(", ")) feltetek = feltetek.Substring(0, feltetek.Length - 2);
+			if (string.IsNullOrEmpty(feltetek)) feltetek = "nincs feltét";
+		}
+	}
 }
